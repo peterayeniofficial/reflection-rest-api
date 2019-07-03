0d978212b4b635a15ae99a83a00f7a9a25e07c5c
Peter Ayeni <peterayeni@Peters-MacBook-Pro.local>
2019-07-03 01:12:19 +0100

    feat: Add API endpoint

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // import express
 const express = require('express')
 import bodyParser from 'body-parser'
+import Reflection from './src/controllers/reflection'
 
 // initialized server
 const app = express()
@@ -10,6 +11,12 @@ app.use(bodyParser())
 app.get('/', (req, res) => {
     return res.status(200).send({'message':'Welcome to our Server'})
 })
+
+app.post('/api/v1/reflections', Reflection.create)
+app.get('/api/v1/reflections', Reflection.getAll)
+app.get('/api/v1/reflections', Reflection.getOne)
+app.put('/api/v1/reflections', Reflection.update)
+app.delete('/api/v1/reflections', Reflection.delete)
 
 app.listen(3000, ()=>{
     console.log('App running on port ', 3000)
